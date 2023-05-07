f105adafae5c6c89b7ea967860e822c6266e278d
Hayden Soo <haydensoo96@gmail.com>
2023-05-07 16:52:17 +0800

    host name

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -2,35 +2,37 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+const host = 'http://localhost:3000'
+
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient) { }
 
   addEmployee(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/create', data);
+    return this._http.post(host + '/create', data);
   }
 
   updateEmployee(userId: string, data: any): Observable<any> {
-    return this._http.post(`http://localhost:3000/update`, {userId, ...data});
+    return this._http.post(host + `/update`, { userId, ...data });
   }
 
   getEmployeeList(): Observable<any> {
-    return this._http.get('http://localhost:3000/user');
+    return this._http.get(host + '/user');
   }
 
-  deleteEmployee( data: any): Observable<any> {
-    const body = {userId: data}
-    return this._http.post(`http://localhost:3000/delete`, body);
+  deleteEmployee(data: any): Observable<any> {
+    const body = { userId: data }
+    return this._http.post(host + `/delete`, body);
   }
 
-  updateSkill( data: any): Observable<any> {
-    return this._http.post(`http://localhost:3000/update/skill`, data);
+  updateSkill(data: any): Observable<any> {
+    return this._http.post(host + `/update/skill`, data);
   }
 
-  updateHobby( data: any): Observable<any> {
+  updateHobby(data: any): Observable<any> {
     console.log('data here', data)
-    return this._http.post(`http://localhost:3000/update/hobby`, data);
+    return this._http.post(host + `/update/hobby`, data);
   }
 }
